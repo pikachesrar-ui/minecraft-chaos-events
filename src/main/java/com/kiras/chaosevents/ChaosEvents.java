7630dd2a0cf1ea066396a2a77742e33c71d221b0
Gug0l <pikaches.rar@gmail.com>
2026-08-01 17:42:17 +0300

    Connect complete chaos engines to server lifecycle

--- src/main/java/com/kiras/chaosevents/ChaosEvents.java
+++ src/main/java/com/kiras/chaosevents/ChaosEvents.java
@@ -2,6 +2,8 @@ package com.kiras.chaosevents;
 
 import com.kiras.chaosevents.command.ChaosCommands;
 import com.kiras.chaosevents.core.ChaosSessionManager;
+import com.kiras.chaosevents.event.BigEventEngine;
+import com.kiras.chaosevents.prank.MicroPrankEngine;
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -9,6 +11,7 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.event.server.ServerStoppingEvent;
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import org.slf4j.Logger;
 
@@ -23,13 +26,21 @@ public final class ChaosEvents {
         NeoForge.EVENT_BUS.addListener(ChaosCommands::onRegisterCommands);
         NeoForge.EVENT_BUS.addListener(this::onServerTick);
 
-        LOGGER.info("Chaos Events loaded");
+        LOGGER.info("Chaos Events loaded: {} big events, {} micro pranks",
+                BigEventEngine.getRegisteredEventCount(),
+                MicroPrankEngine.getRegisteredPrankCount());
     }
 
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         ChaosSessionManager.reset();
         LOGGER.info("Chaos Events: server started; waiting for /chaos start");
+    }
+
+    @SubscribeEvent
+    public void onServerStopping(ServerStoppingEvent event) {
+        ChaosSessionManager.shutdown(event.getServer());
+        LOGGER.info("Chaos Events: server stopping; active mechanics cleaned up");
     }
 
     private void onServerTick(ServerTickEvent.Post event) {
