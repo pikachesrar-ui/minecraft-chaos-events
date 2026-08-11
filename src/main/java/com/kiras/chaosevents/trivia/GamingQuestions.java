e51ba8e28e3705efba5ddf4b82d1db89b1ea3ba1
Gug0l <pikaches.rar@gmail.com>
2026-08-11 15:55:16 +0300

    Add Tarkov and Dunduk trivia questions

---
+++ src/main/java/com/kiras/chaosevents/trivia/GamingQuestions.java
@@ -0,0 +1,69 @@
+package com.kiras.chaosevents.trivia;
+
+import java.util.List;
+
+public final class GamingQuestions {
+    public static final List<TriviaQuestion> QUESTIONS = List.of(
+            new TriviaQuestion("Escape from Tarkov",
+                    "Как называются две основные частные военные компании в Escape from Tarkov?",
+                    List.of("BEAR и USEC", "USEC и BEAR", "bear usec", "usec bear")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Какая компания наняла USEC в мире Escape from Tarkov?",
+                    List.of("TerraGroup", "террагруп", "terra group")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Какая фракция в Escape from Tarkov, по лору, была создана по распоряжению российского правительства?",
+                    List.of("BEAR", "беар")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Как в Escape from Tarkov называют вооружённых местных жителей и мародёров?",
+                    List.of("Scavs", "Scav", "скавы", "скав")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Бывшими бойцами какой ЧВК являются Rogues в Escape from Tarkov?",
+                    List.of("USEC", "юсек")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "На какой карте Rogues охраняют водоочистную станцию?",
+                    List.of("Lighthouse", "маяк")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Как зовут босса, наиболее известного по карте Interchange?",
+                    List.of("Killa", "килла")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Как зовут босса Factory с кувалдой и сварочной маской?",
+                    List.of("Tagilla", "тагилла")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Кем Tagilla приходится Killa по лору Escape from Tarkov?",
+                    List.of("младшим братом", "братом", "младший брат")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Как называется огромный торговый центр на карте Interchange?",
+                    List.of("ULTRA", "ульта", "ультра")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Какая карта была первой добавленной локацией в Escape from Tarkov?",
+                    List.of("Factory", "завод")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Какая фракция в Escape from Tarkov состоит из загадочных последователей культа и использует отравленные ножи?",
+                    List.of("Cultists", "культисты", "сектанты")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Как называется внутренняя элитная спецгруппа TerraGroup?",
+                    List.of("Black Division", "блэк дивижн", "черный дивизион")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Как называется фракция хорошо вооружённых бывших ЧВК и местных бойцов, встречающаяся в рейдах?",
+                    List.of("Scav Raiders", "Raiders", "рейдеры", "скав рейдеры")),
+            new TriviaQuestion("Escape from Tarkov",
+                    "Как называется карта Escape from Tarkov, в центре которой находится торговый комплекс ULTRA?",
+                    List.of("Interchange", "развязка")),
+
+            new TriviaQuestion("Dunduk",
+                    "Как зовут стримера Dunduk?",
+                    List.of("Андрей", "Andrey", "андрей")),
+            new TriviaQuestion("Dunduk",
+                    "Какую игру Dunduk чаще всего стримит и с которой особенно известен?",
+                    List.of("Escape from Tarkov", "Tarkov", "тарков", "eft")),
+            new TriviaQuestion("Dunduk",
+                    "На какой стриминговой платформе известен Dunduk?",
+                    List.of("Twitch", "твич")),
+            new TriviaQuestion("Dunduk",
+                    "В каком году канал Dunduk впервые появился на Twitch?",
+                    List.of("2013", "в 2013", "2013 год"))
+    );
+
+    private GamingQuestions() {
+    }
+}
